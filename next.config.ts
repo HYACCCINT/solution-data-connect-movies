d5c919cc904953838f75244225b1372bfc1d7118
jfschmakeit <jfschmakeit@google.com>
2025-06-19 06:53:08 +0000

    Fix Firebase Authentication sign in pop up issues on Firebase Studio.

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'restrict-properties',
+          },
+        ],
+      },
+    ];
+  },
   transpilePackages: ["genkit"],
   images: {
     remotePatterns: [
